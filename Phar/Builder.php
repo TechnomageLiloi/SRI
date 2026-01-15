85aad68a948f72d3a7652235e998b1552723688e
Anton Moskalenko <moskalenko.anton@gmail.com>
2026-01-15 08:10:24 +0200

    [0.00.003] Add PHAR builder.

---
+++ Phar/Builder.php
@@ -0,0 +1,16 @@
+<?php
+
+    $pharFile = 'SRI.phar';
+
+    if(file_exists($pharFile))
+    {
+        unlink($pharFile);
+    }
+
+    $phar = new Phar($pharFile);
+    $phar->startBuffering();
+    $phar->buildFromDirectory(__DIR__ . '/../Source');
+    $phar->setDefaultStub('Index.php');
+    $phar->stopBuffering();
+    
+    chmod(__DIR__ . "/$pharFile", 0770);
